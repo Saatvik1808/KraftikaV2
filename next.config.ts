a9054544226fd72041800cb790401ce2eae72531
Saatvik <user@ip-192-168-0-103.ap-south-1.compute.internal>
2025-09-20 13:52:31 +0530

    size update

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverComponentsExternalPackages: ['multer'],
+    serverActions: {
+      bodySizeLimit: '800mb',
+    },
   },
   api: {
     bodyParser: {
